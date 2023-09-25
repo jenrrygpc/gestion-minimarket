23550bd546b4baaae22ab81daeb5a8c373eac2e4
Jenrry Guillermo <jenrry.pena.cacya@gmail.com>
2023-09-25 13:08:17 -0500

    build: se cambia directorio a dist

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/measures/', require('./routes/measureRoutes'));
 console.log('antes de servir');
 if (process.env.NODE_ENV === 'production') {
     // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
     console.log('antes de get');
 
     app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/dist/index.html')))
